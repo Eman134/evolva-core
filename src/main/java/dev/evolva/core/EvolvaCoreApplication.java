c3b8f2dec9e159d32e341f33112675eb392fe82c
Eman134 <kaykeeman@gmail.com>
2025-07-09 22:04:41 -0300

    Spring iniciado

---
+++ src/main/java/dev/evolva/core/EvolvaCoreApplication.java
@@ -0,0 +1,13 @@
+package dev.evolva.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolvaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvolvaCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/evolva/core/EvolvaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.evolva.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolvaCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
